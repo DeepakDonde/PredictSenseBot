a7b427e79ebeb2131ed8a9f54d58fa791eefd613
deepakd <deepakd@winjit.com>
2019-03-20 16:30:08 +0530

    added configuration file for bot deployment

--- config.js
+++ config.js
@@ -4,30 +4,30 @@ module.exports = {
     HTTPS_PORT: 443,
 
     CONVEE: {
-        BOT_ID: process.env.BOT_ID,
-        USER_ID: process.env.USER_ID,
-        SERVER_URL: process.env.SERVER_URL,
-        BOT_HOST:"https://"+process.env.BOT_NAME+".herokuapp.com/"
+        BOT_ID: "5c8a1b144565006ba3a95286",
+        USER_ID: "5bd16aacae55ddb9c224e42b",
+        SERVER_URL: "https://convee.ai:1992",
+        BOT_HOST:"https://predictsensebot.azurewebsites.net"
     },
     ADAPTER: {
         FACEBOOK: {
-            ENABLED: process.env.FACEBOOK_ENABLED,
+            ENABLED: true,
             MESSENGER_WEBHOOK:"/fb_webhook",
-            MESSENGER_APP_ID: process.env.MESSENGER_APP_ID,
-            MESSENGER_ACCESS_TOKEN:process.env.MESSENGER_ACCESS_TOKEN,
-            MESSENGER_APP_SECRET: process.env.MESSENGER_APP_SECRET,
+            MESSENGER_APP_ID: "413262699438631",
+            MESSENGER_ACCESS_TOKEN:"EAAF33DhmIicBANXoPOdGJZC3cuBW4Y4jgSYS3pIEFcq91oUe4GwcJElAyMTWgGDMlbzUdzojIbRW4kL8LKolRjfVQ0OA5d9EyMPBGiiPDO9CFQUbj76B64L51LuvQngKllBgFYLLE6vytmhi1r9UemhY1XI2O5MA9ooZAwsgZDZD",
+            MESSENGER_APP_SECRET: "e4fdb8305dd31dec88959eb83ba9e42c",
             MESSENGER_VERIFY_TOKEN: "convee_adapter"
         },
 
         TELEGRAM:{
-            ENABLED: process.env.TELEGRAM_ENABLED,
+            ENABLED: false,
             MESSENGER_WEBHOOK:"/tg_webhook",
-            TOKEN:process.env.TELEGRAM_TOKEN
+            TOKEN:""
         },
 
         ALEXA: {
-            ENABLED: process.env.ALEXA_ENABLED,
-            SKILL_ID: process.env.ALEXA_SKILL_ID,
+            ENABLED: true,
+            SKILL_ID: "amzn1.ask.skill.06053975-77bc-42d0-9393-6853a0da2583",
             ALEXA_WEBHOOK: "/alexa_adapter"
         }
 
